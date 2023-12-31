d46614f9a9ab87d8edef139421794811e19b06f3
patwary88 <mainuddinpatwary1988@gmail.com>
2023-12-31 23:58:40 +0600

    add class for whole application when access mobile device

--- pages/home.php
+++ pages/home.php
@@ -3,18 +3,19 @@
 <p>Add multiple inputs or addons:</p>
 
 <!-- Multiple inputs -->
-<form id="emp_basic">
+<form id="emp_basic" autocomplete="off">
   <div class="row">
     <div class="col input-group">
-      <span class="input-group-text">Person</span>
-      <input type="text" class="form-control" placeholder="First Name" id="first_name">
+      <span class="input-group-text" id="inputGroup-sizing-sm">First Name</span>
+      <input type="text" id="first_name" name="first_name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
     </div>
     <div class="col input-group">
-      <span class="input-group-text">Person</span>
-      <input type="text" class="form-control" placeholder="First Name" id="last_name">
+      <span class="input-group-text" id="inputGroup-sizing-default">Middle Name</span>
+      <input type="text" id="middle_name" name="middle_name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+    <div class="col input-group">
+      <span class="input-group-text" id="inputGroup-sizing-default">Last Name</span>
+      <input type="text" id="last_name" name="last_name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
     </div>
   </div>
 </form>
-
-
-<script type="text/javascript"></script>
