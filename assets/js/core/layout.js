e78eb4917b47efd08438e45318c2574af9b6332a
patwary88 <mainuddinpatwary1988@gmail.com>
2024-01-01 23:40:24 +0600

    introduce select2 for form select and multiselect

--- assets/js/core/layout.js
+++ assets/js/core/layout.js
@@ -119,26 +119,6 @@ $(document).ready(function () {
       window.addEventListener('resize', updateInputGroupClass);
     }
 
+    $('.cbo_blood_group_id').selectpicker();
+
  });
-
-//  $(document).ready(function() {
-
-//     // Function to apply the input-group-sm class to all elements with class "input-group" based on screen width
-//     function updateInputGroupClass() {
-//       var inputGroups = document.querySelectorAll('.input-group');
-
-//       inputGroups.forEach(function(inputGroup) {
-//         if (isMobile()) {
-//           inputGroup.classList.add('input-group-sm');
-//         } else {
-//           inputGroup.classList.remove('input-group-sm');
-//         }
-//       });
-//     }
-
-//     // Initial check on page load
-//     updateInputGroupClass();
-
-//     // Update the class when the window is resized
-//     window.addEventListener('resize', updateInputGroupClass);
-//   });     
--- pages/home.php
+++ pages/home.php
@@ -4,18 +4,85 @@
 
 <!-- Multiple inputs -->
 <form id="emp_basic" autocomplete="off">
+<div class="row">
+    <div class="col input-group mb-2">
+      <span class="input-group-text" id="inputGroup-sizing-sm">Emp Code</span>
+      <input type="text" id="txt_emp_code" name="txt_emp_code" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+    </div>
+    <div class="col input-group mb-2">
+      <span class="input-group-text" id="inputGroup-sizing-default">Card No</span>
+      <input type="text" id="txt_id_card_no" name="txt_id_card_no" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+    <div class="col input-group mb-2">
+      <span class="input-group-text" id="inputGroup-sizing-default">Punch No</span>
+      <input type="text" id="txt_punch_card_no" name="txt_punch_card_no" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+  </div>
   <div class="row">
-    <div class="col input-group">
+    <div class="col input-group mb-2">
       <span class="input-group-text" id="inputGroup-sizing-sm">First Name</span>
-      <input type="text" id="first_name" name="first_name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+      <input type="text" id="txt_first_name" name="txt_first_name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
     </div>
-    <div class="col input-group">
+    <div class="col input-group mb-2">
       <span class="input-group-text" id="inputGroup-sizing-default">Middle Name</span>
-      <input type="text" id="middle_name" name="middle_name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+      <input type="text" id="txt_middle_name" name="txt_middle_name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
     </div>
-    <div class="col input-group">
+    <div class="col input-group mb-2">
       <span class="input-group-text" id="inputGroup-sizing-default">Last Name</span>
-      <input type="text" id="last_name" name="last_name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+      <input type="text" id="txt_last_name" name="txt_last_name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col input-group mb-2">
+      <span class="input-group-text" id="inputGroup-sizing-sm">Name ( Bangla ) </span>
+      <input type="text" id="txt_full_name_bangla" name="txt_full_name_bangla" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+    </div>
+    <div class="col input-group mb-2">
+      <span class="input-group-text" id="inputGroup-sizing-default">Father Name</span>
+      <input type="text" id="txt_father_name" name="txt_father_name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+    <div class="col input-group mb-2">
+      <span class="input-group-text" id="inputGroup-sizing-default">Father Name ( Bangla )</span>
+      <input type="text" id="txt_father_name_bangla" name="txt_father_name_bangla" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col input-group mb-2">
+      <span class="input-group-text" id="inputGroup-sizing-sm">Mother Name </span>
+      <input type="text" id="txt_mother_name" name="txt_mother_name" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+    </div>
+    <div class="col input-group mb-2">
+      <span class="input-group-text" id="inputGroup-sizing-default">Mother Name ( Bangla )</span>
+      <input type="text" id="txt_mother_name_bangla" name="txt_mother_name_bangla" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+    <div class="col input-group mb-2">
+      <span class="input-group-text" id="inputGroup-sizing-default">Date Of Birth</span>
+      <input type="date" id="txt_date_of_birth" name="txt_date_of_birth" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col input-group mb-2">
+      <span class="input-group-text" id="inputGroup-sizing-sm">Blood Group </span>
+      <!-- <input type="text" id="cbo_blood_group_id" name="cbo_blood_group_id" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"> -->
+      <select class="form-select" data-live-search="true" id="cbo_blood_group_id">
+        <option>Option 1</option>
+        <option>Option 2</option>
+        <option>Option 3</option>
+        <option>Option 4</option>
+        <option>Option 5</option>
+      </select>
+
+    </div>
+    <div class="col input-group mb-2">
+      <span class="input-group-text" id="inputGroup-sizing-default">Nationality</span>
+      <input type="text" id="cbo_nationality_id" name="cbo_nationality_id" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+    <div class="col input-group mb-2">
+      <span class="input-group-text" id="inputGroup-sizing-default">Religion</span>
+      <input type="text" id="cbo_religion_id" name="cbo_religion_id" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
     </div>
   </div>
 </form>
+<script>
+  
+</script>
